13f8fe82ee15eea5a136c39b1406d3fccb510bb2
Hlao Khang <khangh@gmatc.matc.edu>
2023-10-07 21:17:46 -0500

    Connected C# backend to Angular frontend

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,24 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'client';
+  title = 'new title';
+  weatherForescasts: any;
+
+  constructor(private http: HttpClient) {}
+  ngOnInit(): void {
+    this.http.get('http://localhost:5297/weatherforecast').subscribe(
+      (response) => {
+        this.weatherForescasts = response;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
 }
