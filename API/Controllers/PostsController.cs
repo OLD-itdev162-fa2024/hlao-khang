9374dbc2a93911459e318c5ae3eceb8aa696af2c
Hlao Khang <khangh@gmatc.matc.edu>
2023-10-22 20:17:59 -0500

    Created second end oint

--- API/Controllers/PostsController.cs
+++ API/Controllers/PostsController.cs
@@ -22,5 +22,15 @@ namespace API.Controllers{
     public ActionResult<List<Post>> Get(){
       return this.context.Posts.ToList();
     }
+
+    [HttpGet("{id}", Name = "GetById")]
+    public ActionResult<Post> GetById(Guid id){
+      var post = this.context.Posts.Find(id);
+      if(post is null){
+        return NotFound();
+      }
+
+      return Ok(post);
+    }
   }
 }
