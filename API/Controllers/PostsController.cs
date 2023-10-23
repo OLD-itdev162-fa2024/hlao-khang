3206a1e5fccf881ab0d7a4d056f5336ce5b7d010
Hlao Khang <khangh@gmatc.matc.edu>
2023-10-22 20:38:34 -0500

    Added 3rd api endpoint

--- API/Controllers/PostsController.cs
+++ API/Controllers/PostsController.cs
@@ -4,13 +4,13 @@ using System.ComponentModel;
 using System.Linq;
 using System.Threading.Tasks;
 using Domain;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using Persistence;
 
 namespace API.Controllers{
   [ApiController]
   [Route("api/[controller]")]
-
   public class PostsController : ControllerBase{
     private readonly DataContext context;
     public PostsController(DataContext context){
@@ -23,6 +23,11 @@ namespace API.Controllers{
       return this.context.Posts.ToList();
     }
 
+    // <summary>
+    // Get api/post[id]
+    // </summary>
+    // parm nam = id
+    // returns single post
     [HttpGet("{id}", Name = "GetById")]
     public ActionResult<Post> GetById(Guid id){
       var post = this.context.Posts.Find(id);
@@ -31,6 +36,24 @@ namespace API.Controllers{
       }
 
       return Ok(post);
+    }
+      
+    [HttpPost(Name = "Create")]
+    public ActionResult<Post> Create([FromBody]Post request){
+      var post = new Post{
+        Id = request.Id,
+        Title = request.Title,
+        Body = request.Body,
+        Date = request.Date
+      };
+
+      context.Posts.Add(post);
+      var success = context.SaveChanges() > 0;
+      if(success){
+        return Ok(post);
+      }
+
+      throw new Exception("Error creating post");
     }
   }
 }
