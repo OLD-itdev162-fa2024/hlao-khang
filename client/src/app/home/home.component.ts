f9fe85bdad1c70e44649fe208ad489de9f13788e
Hlao Khang <khangh@gmatc.matc.edu>
2023-12-04 17:09:35 -0600

    Home component finished

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,15 +1,24 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  posts: any;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
+    this.http.get('http://localhost:5297/api/posts').subscribe(
+      (response) => {
+        this.posts = response;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
   }
-
 }
